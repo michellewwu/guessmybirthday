0762e8c0582e13bee1e80f4ceddebd2b85daa667
gracee9 <gmahathananont@madeira.org>
2021-12-17 21:33:46 +0700

    function added

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import math
 import random
+
 def guess_birthday(month,d,t):
       l=[]
       if month=="December" or month=="January" or month=="March" or month=="May" or month=="July" or month=="August" or month=="October": 
@@ -10,112 +10,58 @@ def guess_birthday(month,d,t):
       if(d=='odd' and t=='before'): l=[1, 3, 5, 7, 9, 11, 13, 15]
       elif(d=='even' and t=='after'): l=[18, 20, 22, 24, 26, 28, 30]
       elif(d=='even' and t=='before'): l=[2, 4, 6, 8, 10, 12, 14, 16]
-
       
+      day=str((random.choice(l)))
+      m = input("Is your birthday "+month+" "+day+"? (yes/no)")
+      while m.lower() == 'no':
+        day=str((random.choice(l)))
+        m = input("Is your birthday "+month+" " +day+ "?(yes/no)")
+        if m.lower() == 'yes':
+          return month,day
+      return month,day
 
-      
-      o = (print("Is your birthday "+month+" ", (random.choice(l)), "?"))
-      while input(o.lower()) != "Yes":
-          print("Is your birthday "+month+" ", (random.choice(l)), "?")
-
-'''def daywith31():
-d = input("Is your birth date odd or even? ")
-if d == 'odd':
-print
-print (random.randint(odda16w31))
-'''
-print ("Hello! Welcome to the 'Guess My Birthday' game!")
-sea = input("Please start by answering this question: Which season is your birthday in? ")
-
-if sea.lower() == 'fall':
-  f = input("Is your birthday in the same month as Labor Day, Halloween, or Thanksgiving? ")
-  if f.lower() == 'labor day':
-    d = input("Is your birth date odd or even? ")
-    if d.lower() == 'odd':
-      w = input("Is your birth date before or after the 16th? ")
-      if w.lower() == 'after the 16th' or w.lower() == 'after':
-        o = (print("Is your birthday September ", (random.choice(odda16)), "?"))
-        while input(o.lower()) != "Yes":
-          print("Is your birthday September ", (random.choice(odda16)), "?")
-  elif f.lower()=="":
-    pass
-  else:
-    pass
-
-elif sea.lower() == 'spring':
-    sp = input("Is your birthday in the same month as St.Patrick's Day, Easter, or Mother's Day?")
-
-elif sea.lower() == 'summer':
-    su = input("Is your birthday in the same month as Father's Day, Fourth of July, or National Watermelon Day?")
-
-elif sea.lower()=='winter':
+def main():
+  sea = input("Please start by answering this question: Which season is your birthday in?")
+  print()
+  if sea.lower() == 'winter':
     w = input("Is your birthday in the same month as Christmas, New Years, or Valentine's Day?")
     if w.lower() == 'christmas':
-      d = input("Is your birth date odd or even? ")
-      if d.lower() == 'odd':
-        w = input("Is your birth date before or after the 16th? ")
-        if w.lower() == 'after the 16th' or w.lower() == 'after':
-          o = (print("Is your birthday Decmeber ", (random.choice(odda16)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday December ", (random.choice(odda16)), "?")
-        elif w.lower() == 'after the 16th' or w.lower() == 'before':
-          o = (print("Is your birthday Decmeber ", (random.choice(oddb16)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday December ", (random.choice(oddb16)), "?")
-      elif d.lower() == 'even':
-        w = input("Is your birth date before or after the 17th? ")
-        if w.lower() == 'after the 17th' or w.lower() == 'after':
-          o = (print("Is your birthday December ", (random.choice(evena17)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday December ", (random.choice(evena17)), "?")
-        elif w.lower() == 'after the 17th' or w.lower() == 'before':
-          o = (print("Is your birthday Decmeber ", (random.choice(evenb17)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday December ", (random.choice(evenb17)), "?")
-    elif w.lower() == 'new years':
-      d = input("Is your birth date odd or even? ")
-      if d.lower() == 'odd':
-        w = input("Is your birth date before or after the 16th? ")
-        if w.lower() == 'after the 16th' or w.lower() == 'after':
-          o = (print("Is your birthday January ", (random.choice(odda16)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday January ", (random.choice(odda16)), "?")
-        elif w.lower() == 'after the 16th' or w.lower() == 'before':
-          o = (print("Is your birthday January ", (random.choice(oddb16)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday January ", (random.choice(oddb16)), "?")
-      elif d.lower() == 'even':
-        w = input("Is your birth date before or after the 17th? ")
-        if w.lower() == 'after the 17th' or w.lower() == 'after':
-          o = (print("Is your birthday January ", (random.choice(evena17)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday January ", (random.choice(evena17)), "?")
-        elif w.lower() == 'after the 17th' or w.lower() == 'before':
-          o = (print("Is your birthday January ", (random.choice(evenb17)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday January ", (random.choice(evenb17)), "?")
-    elif w.lower() == 'valentine':
-      d = input("Is your birth date odd or even? ")
-      if d.lower() == 'odd':
-        w = input("Is your birth date before or after the 16th? ")
-        if w.lower() == 'after the 16th' or w.lower() == 'after':
-          o = (print("Is your birthday February ", (random.choice(odda16)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday February ", (random.choice(odda16)), "?")
-        elif w.lower() == 'after the 16th' or w.lower() == 'before':
-          o = (print("Is your birthday February ", (random.choice(oddb16)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday February ", (random.choice(oddb16)), "?")
-      elif d.lower() == 'even':
-        w = input("Is your birth date before or after the 17th? ")
-        if w.lower() == 'after the 17th' or w.lower() == 'after':
-          o = (print("Is your birthday February ", (random.choice(evena17)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday February ", (random.choice(evena17)), "?")
-        elif w.lower() == 'after the 17th' or w.lower() == 'before':
-          o = (print("Is your birthday February ", (random.choice(evenb17)), "?"))
-          while input(o.lower()) != "Yes":
-            print("Is your birthday February ", (random.choice(evenb17)), "?")
-
-
-
+      month='December'
+    elif w.lower() =='new years':
+      month='January'
+    elif w.lower() == "valentine's day":
+      month='February'
+  elif sea.lower() == 'fall':
+    w = input("Is your birthday in the same month as Labor Day, Halloween, or Thanksgiving?")
+    if w.lower() == 'labor Day':
+      month='September'
+    elif w.lower() =='halloween':
+      month='October'
+    elif w.lower() == 'thanksgiving':
+      month='November'
+  elif sea.lower() =='spring':
+    w = input("Is your birthday in the same month as St. Patricks, Easter, or Mother's day?")
+    if w.lower() =='st. patricks':
+      month = 'March'
+    elif w.lower() =='easter':
+      month='April'
+    elif w.lower() == "mother's day":
+      month='May'
+  elif sea.lower() == 'summer':
+    w = input("Is your birthday in the same month as Father's day, Fourth of July, or National Watermelon Day?")
+    if w.lower() =="father's day":
+      month = 'June'
+    elif w.lower() =='fourth of july':
+      month='July'
+    elif w.lower() == "national watermelon day":
+      month='August'
+  
+  d = input("Is your birth date odd or even? (odd/even)")
+  if d=='odd':
+    t = input("Is your birth date before or after the 16th? (before/after)")
+  elif d=='even':
+    t = input("Is your birth date before or after the 17th? (before/after)")
+  m,day=guess_birthday(month,d,t)
+  print("I guessed it! Your Birthday is" + " "+m +" "+ day )
+     
+main()
